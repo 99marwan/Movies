ae497c44d32dbc1ffb3d44672b32af3cf3d4ac81
Marwan Abougabal <54478282+99marwan@users.noreply.github.com>
2023-03-11 00:06:20 +0200

    mongoDB
    * new branch
    
    * mongo modifications
    
    * mongo modifications

--- movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
@@ -2,7 +2,6 @@ package com.example.movieinfoservice.resources;
 
 import com.example.movieinfoservice.models.Movie;
 import com.example.movieinfoservice.models.MovieSummary;
-// import com.example.movieinfoservice.services.CacheService;
 import com.example.movieinfoservice.services.MovieRowMapper;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -10,25 +9,16 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import static com.mongodb.client.model.Filters.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-// import java.util.HashMap;
-// import java.util.Map;
-
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
-    // @Autowired
-    // private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
     MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
     MongoDatabase database = mongoClient.getDatabase("movie_cache");
     MongoCollection<Document> collection = database.getCollection("movies");
@@ -44,13 +34,6 @@ public class MovieResource {
     }
 
     private void cacheData(String movieId, String name, String description) {
-        // String sql = "INSERT INTO movie_cache (movieId, name, description) VALUES (:movieId, :name, :description)";
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("movieId", movieId);
-        // params.put("name", name);
-        // params.put("description", description);
-
-        // namedParameterJdbcTemplate.query(sql, params, new MovieRowMapper());
         Document doc = new Document("movieId", movieId)
                 .append("name", name)
                 .append("description", description);
@@ -60,12 +43,8 @@ public class MovieResource {
 
     private Movie getCachedData(String movieId) {
         Document doc = collection.find(eq("movieId", movieId)).first();
-
-        // String sql = "SELECT movieId, name, description FROM movie_cache WHERE movieId = :movieId";
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("movieId", movieId);
-        // Movie movie = namedParameterJdbcTemplate.queryForObject(sql, params, new MovieRowMapper());
-
+        if (doc == null)
+            return null;
         Movie movie = new MovieRowMapper().getMovie(doc);
         return movie;
     }
@@ -73,16 +52,13 @@ public class MovieResource {
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
         Movie CachedData = getCachedData(movieId);
-        if (CachedData != null) {
-
+        if (CachedData != null)
             return CachedData;
-        }
+
         // Get the movie info from TMDB
         final String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey;
         MovieSummary movieSummary = restTemplate.getForObject(url, MovieSummary.class);
-
         cacheData(movieId, movieSummary.getTitle(), movieSummary.getOverview());
-
         return getCachedData(movieId);
     }
 }
